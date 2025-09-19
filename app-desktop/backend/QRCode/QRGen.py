6903f3be422b410cd595f47ade5ded5f98dc63b5
Murthy9644 <bvjsmurthy90@gmail.com>
2025-09-19 20:38:28 +0530

    QR Scree GUI beta

--- app-desktop/backend/QRCode/QRGen.py
+++ app-desktop/backend/QRCode/QRGen.py
@@ -3,7 +3,7 @@ import qrcode, json
 class QRGenerator:
 
     def generator(self):
-        qrcode.make(json.dumps(self.data)).save("app-desktop/backend/QRCode/TIDqr.png")
+        qrcode.make(json.dumps(self.data)).save(f"app-desktop/backend/QRCode/{self.data["tid"]}.png")
 
     def __init__(self, data):
         self.data = data
